ddebc1f623186e1da80ace0ef73e7e199247dcc9
blackholdconsulting <info@blackholdconsulting.com>
2025-08-13 12:32:38 +0200

    Update SignerApplication.java

--- src/main/java/com/clientum/signer/SignerApplication.java
+++ src/main/java/com/clientum/signer/SignerApplication.java
@@ -1,1 +1,11 @@
+package com.clientum.signer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SignerApplication.class, args);
+  }
+}
